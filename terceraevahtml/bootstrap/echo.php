ab4bd391a9fcadeb5f811471c46465d05625b6d4
JosebaVazUrkiza21 <56816898+josebavazurkiza@users.noreply.github.com>
2020-03-31 16:13:56 +0200

    Add files via upload

---
+++ terceraevahtml/bootstrap/echo.php
@@ -0,0 +1,40 @@
+<?php
+
+/*echo "<h1>";*/
+if (isset($_GET["ciudad"]))
+    echo $_GET["ciudad"];
+/*echo "</h1>";*/
+echo "<br>";
+if (isset($_GET["viajas"]))
+    echo $_GET["viajas"];
+
+echo "<br>";
+if (isset($_GET["email"]))
+    echo $_GET["email"];
+
+echo "<br>";
+if (isset($_GET["contra"]))
+    echo $_GET["contra"];
+        
+echo "<br>";
+if (isset($_GET["patata"]))
+    echo $_GET["patata"];
+
+        
+echo "<br>";
+if (isset($_GET["tomate"]))
+    echo $_GET["tomate"];
+    
+        
+echo "<br>";
+if (isset($_GET["lechuga"]))
+    echo $_GET["lechuga"];
+            
+echo "<br>";
+if (isset($_GET["comentario"]))
+    echo $_GET["comentario"];
+    
+            
+echo "<br>";
+if (isset($_GET["info"]))
+    echo $_GET["info"];
